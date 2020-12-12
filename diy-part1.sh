38a6101e7c6d5b7ee403f8b57e6f681802821145
stonegr <74632994+stonegr@users.noreply.github.com>
2020-12-12 10:37:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,10 @@
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/chenhw2/luci-app-koolproxy.git package/luci-app-koolproxy
 git clone https://github.com/pymumu/smartdns.git package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
