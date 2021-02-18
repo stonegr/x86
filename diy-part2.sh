796a75f4d94b07290a72d2d46ee42ff054cc0f83
stonegr <74632994+stonegr@users.noreply.github.com>
2021-02-18 18:14:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-sed -i 's/#CONFIG_X86_DEBUG_FPU is not set/CONFIG_X86_DEBUG_FPU=y/g' target/linux/x86/config-5.4
+sed -i 's/# CONFIG_X86_DEBUG_FPU is not set/CONFIG_X86_DEBUG_FPU=y/g' target/linux/x86/config-5.4
 sed -i 's/root::0:0:99999:7:::/root:$1$ZzLkZmEb$Kpkpyxaj6bYCEtrs7LqWs.:18612:0:99999:7:::/g' package/base-files/files/etc/shadow
 rm -rf feeds/packages/net/smartdns
